db5d17057d59ad9fe512021c74c965f1381e5998
Kirk Woll <kirk.woll@gmail.com>
2017-11-05 11:41:38 -0800

    Add a script variant for mock http handlers

---
+++ SexyHttp/Mocks/MockHttpHandlerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SexyHttp.Mocks
+{
+    public class MockHttpHandlerScript : IHttpHandler
+    {
+        private readonly Queue<IHttpHandler> script = new Queue<IHttpHandler>();
+
+        public void Add(IHttpHandler handler)
+        {
+            script.Enqueue(handler);
+        }
+
+        public Task<HttpHandlerResponse> Call(HttpApiRequest request)
+        {
+            var handler = script.Dequeue();
+            return handler.Call(request);
+        }
+    }
+}
